cbc0219f461a7fc96ae830e08982500333c6ec68
Tashif Ahmad Khan <tashifkhan010@gmail.com>
2026-01-16 10:37:37 +0530

    updates cors

--- server/app/__init__.py
+++ server/app/__init__.py
@@ -55,8 +55,8 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:3000",
         "http://localhost:5173",
-        "https://*.vercel.app",
-        "https://*.tashif.codes",
+        "https://paisa-three.vercel.app",
+        "https://paisa.tashif.codes",
     ],
     allow_credentials=True,
     allow_methods=["*"],
