60600c125dfb5bf3c65d5f9f584de87342f224bb
Tashif Ahmad Khan <tashifkhan010@gmail.com>
2026-07-20 22:39:23 +0530

    chore: drop unused imports in RecordsScreen

--- app/src/main/java/codes/tashif/paisa/ui/screens/RecordsScreen.kt
+++ app/src/main/java/codes/tashif/paisa/ui/screens/RecordsScreen.kt
@@ -1,6 +1,5 @@
 package codes.tashif.paisa.ui.screens
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Arrangement
@@ -60,7 +59,6 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.graphics.vector.ImageVector
